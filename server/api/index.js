5b1f4d5a2cb965c6276f0a297d4015d96cc15c65
Jeff Gore <jgore00@gmail.com>
2018-08-12 23:11:07 -0400

    refactored Virtual Radar Server request

--- server/api/index.js
+++ server/api/index.js
@@ -2,18 +2,25 @@
 const router = require('express').Router();
 const request = require('request');
 const { createFleetFromAircraftList } = require('../utils');
-const radarServerURI = 'https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json';
-
-let fleet;
+const radarServerURI =
+  'https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json';
 
 router.get('/:airline', (req, res, next) => {
   request(
-    `${radarServerURI}?fCallS=${req.params.airline}`, 
+    `${radarServerURI}?fCallS=${req.params.airline}`,
     (apiErr, apiRes, apiBody) => {
-      const data = JSON.parse(apiBody);
-      fleet = createFleetFromAircraftList(data.acList);
-      res.send(fleet);
-    });
+      if (apiErr) {
+        res
+          .status(500)
+          .send(
+            'There was a problem getting data from the Virtual Radar Server.'
+          );
+      } else {
+        const { acList } = JSON.parse(apiBody);
+        res.send(createFleetFromAircraftList(acList));
+      }
+    }
+  );
 });
 
 module.exports = router;

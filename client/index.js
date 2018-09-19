b2b2a06d14296efdc91ba8bf09586d10bf4e569b
Jeff Gore <jgore00@gmail.com>
2018-09-19 12:07:33 -0400

    working splash screen, still need to add window resize event handlers

--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
 import store, { fetchInitialFleet } from './store';
 import { addFunctionalityToButtons } from './menu/selectionButtons';
 
-addFunctionalityToButtons();
-store.dispatch(fetchInitialFleet('DAL'));
+const header = document.querySelector('header');
+const splash = document.getElementById('mobileSplash');
+
+if (window.innerWidth < 900) {
+  header.style.display = 'none';
+  splash.style.display = 'inline';
+} else {
+  addFunctionalityToButtons();
+  store.dispatch(fetchInitialFleet('DAL'));
+}

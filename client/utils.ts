d7690c1f2dfa6da30a1b9397e9b9029b5eb78378
Jeff Gore <jgore00@gmail.com>
2018-10-20 18:39:25 -0400

    add Airport and Aiirplane TS interfaces

--- client/utils.ts
+++ client/utils.ts
@@ -1,10 +1,36 @@
 /* Code courtesy of https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript */
 
-interface reduxStore {
+export interface ReduxStore {
   carrier: string;
-  fleet: any[];
+  fleet: Airplane[];
   queryIntervalId: number;
   getState: Function;
+}
+
+export interface Airplane {
+  callsign: string;
+  altitude: number;
+  grounded: boolean;
+  speed: number;
+  lat: number;
+  long: number;
+  heading: number;
+  aircraftType: string;
+  airportFrom: Airport;
+  airportTo: Airport;
+  airportStops: Airport[];
+  ageInYears: number;
+  flightPercentComplete: number;
+}
+
+export interface Airport {
+  code: string;
+  lat: number;
+  long: number;
+  name: string;
+  city: string;
+  state: string;
+  country: string;
 }
 
 export const numberWithCommas = (num: number): string => {
@@ -12,7 +38,7 @@ export const numberWithCommas = (num: number): string => {
 };
 /* */
 
-export const getFirstCallsign = (reduxStore: reduxStore): string => {
+export const getFirstCallsign = (reduxStore: ReduxStore): string => {
   const fleet = reduxStore.getState().fleet;
   return fleet[0] ? fleet[0].callsign.slice(0, 3) : "";
 };

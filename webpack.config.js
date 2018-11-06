00fead69d9c8d0bd4bceb6be147e19d2be7dace9
Jeff Gore <jgore00@gmail.com>
2018-11-06 18:27:36 -0500

    add babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   devtool: 'source-map',
-  entry: './client/index.js',
+  entry: ['@babel/polyfill', './client/index.js'],
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'

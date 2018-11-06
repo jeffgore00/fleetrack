d5e44940c7ed134e32379231eebba8efecfbba87
Jeff Gore <jgore00@gmail.com>
2018-11-06 17:51:43 -0500

    more updates to babel configuration

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 'use strict';
-
+require('@babel/polyfill');
 const app = require('./server');
 const { getAirports } = require('./server/utils');
 const PORT = process.env.PORT || 1337;
 
 getAirports()
-  .then( () => {
+  .then(() => {
     app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
   })
   .catch(err => console.log(err));
-

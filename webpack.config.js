1a4fa8850e9b2bf6429ffe2c8ae92145c06c5b01
Jeff Gore <jgore00@gmail.com>
2018-11-07 08:35:06 -0500

    more babel updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 module.exports = {
   devtool: 'source-map',
-  entry: ['@babel/polyfill', './client/index.js'],
+  entry: './client/index.js', // rather than
+  // ['@babel/polyfill', './client/index.js'], see:
+  // https://github.com/babel/website/pull/1858/commits/6e510c60946eb2338c3895c04e04c07bfcf79901
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'

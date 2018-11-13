f8d28497a2bf8d04dffb90cdc5f83777b9afef62
Jeff Gore <jgore00@gmail.com>
2018-11-13 12:55:31 -0500

    update server boilerplate to refer to a database

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 'use strict';
-const app = require('./server');
+const { app, createApp } = require('./server');
 const { getAirports } = require('./server/utils');
+const db = require('./db');
 const PORT = process.env.PORT || 1337;
 
-getAirports()
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
-  })
-  .catch(err => console.log(err));
+const startListening = () => {
+  app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+};
+
+const syncDb = () => db.sync();
+
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  getAirports
+    .then(syncDb)
+    .then(createApp)
+    .then(startListening);
+} else {
+  createApp();
+}
--- server/index.js
+++ server/index.js
@@ -4,29 +4,32 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
 const app = express();
 
-//logging middleware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') require('../../secrets');
 
-//body parsing middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const createApp = () => {
+  //logging middleware
+  app.use(morgan('dev'));
 
-//static middleware
-app.use(express.static(path.join(__dirname, '../public')));
+  //body parsing middleware
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api', require('./api')); // include our routes!
+  //static middleware
+  app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-}); // Send index.html for any other requests
+  app.use('/api', require('./api')); // include our routes!
 
-//error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error');
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  }); // Send index.html for any other requests
 
-module.exports = app;
+  //error handling middleware
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error');
+  });
+};
+
+module.exports = { app, createApp };
